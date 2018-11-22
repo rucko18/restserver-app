a6120c0742f190c5f9bb485a0a56770007347744
Eduardo Torrez <38381517+rucko18@users.noreply.github.com>
2018-11-21 20:59:19 -0600

    RESTServer - Usuario

--- server/config/config.js
+++ server/config/config.js
@@ -1,4 +1,24 @@
 // ==============================
-// puerto
+//      PUERTO
 // ==============================
 process.env.PORT = process.env.PORT || 3000;
+
+
+// ==============================
+//      ENTORNO
+// ==============================
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+// ==============================
+//      ENTORNO
+// ==============================
+
+let urlDB;
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb://cafe-user:*admin1@ds161630.mlab.com:61630/cafedb';
+}
+
+process.env.URLDB = urlDB;
