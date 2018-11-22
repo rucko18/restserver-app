04266808d83d6742fe8a5dac277376160788185a
Eduardo Torrez <38381517+rucko18@users.noreply.github.com>
2018-11-21 21:29:21 -0600

    Variable de Entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -18,7 +18,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb://cafe-user:*admin1@ds161630.mlab.com:61630/cafedb';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
